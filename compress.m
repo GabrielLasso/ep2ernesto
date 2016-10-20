f6d01a297778591cbe17244d7485604bf4bcbb19
GabrielLasso <gabriel.lasso@usp.br>
2016-10-20 16:59:22 -0200

    nome como argumento

--- compress.m
+++ compress.m
@@ -1,4 +1,5 @@
-function compress (OriginalImg, k)
+function compress (ImgName, k)
+  OriginalImg = imread (ImgName);
   for x = 0:size(OriginalImg)(1)/(k+1)-1
     for y = 0:size(OriginalImg)(2)/(k+1)-1
       I(x+1,y+1,:) = OriginalImg (x*(k+1)+1,y*(k+1)+1,:);
