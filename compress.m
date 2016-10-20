2b13adc4f2a4c19ba7779c043aa14df4bd41c524
andrevic <andrevic@users.noreply.github.com>
2016-10-20 17:24:10 -0200

    Mudança de nome dos argumentos

--- compress.m
+++ compress.m
@@ -1,5 +1,5 @@
-function compress (ImgName, k)
-  OriginalImg = imread (ImgName);
+function compress (OriginalImg, k)
+  OriginalImg = imread (OriginalImg);
   for x = 0:size(OriginalImg)(1)/(k+1)-1
     for y = 0:size(OriginalImg)(2)/(k+1)-1
       I(x+1,y+1,:) = OriginalImg (x*(k+1)+1,y*(k+1)+1,:);
